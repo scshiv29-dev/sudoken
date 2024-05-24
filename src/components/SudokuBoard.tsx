153effbb56a0d8fa065baf233ff0b18c624c1ca1
Rhemaria01 <reenavhemaria789@gmail.com>
2024-05-24 14:24:42 +0530

    le tera board fix kr deeya

--- src/components/SudokuBoard.tsx
+++ src/components/SudokuBoard.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React, { useState } from "react";
 import { cn } from "@/lib/utils";
-export default function SudokuBoard({ sudokudata,sudokuSolution,stopTimer }: any) {
+export default function SudokuBoard({
+  sudokudata,
+  sudokuSolution,
+  stopTimer,
+}: any) {
   // Initialize the state with the initial Sudoku data
   const [board, setBoard] = useState(sudokudata);
 
@@ -24,24 +28,34 @@ export default function SudokuBoard({ sudokudata,sudokuSolution,stopTimer }: any
 
   return (
     <div className="flex justify-center items-center">
-      <div className="grid grid-cols-9 gap-1 p-4 shadow-lg rounded-lg">
+      <div className="grid grid-cols-21  p-4 shadow-lg rounded-lg">
         {board.map((row: any[], rowIndex: number) =>
-        
           row.map((cell, cellIndex) => {
-            const thirdcell=(cellIndex+1)%3===0
-            console.log(typeof(cellIndex))
-            return (  
-              <input
-                key={`${rowIndex+1}-${cellIndex+1}`}
-                type="text"
-                value={cell === "0" ? "" : cell}
-                readOnly={sudokudata[rowIndex][cellIndex] !== "0"}
-                onChange={(e) =>
-                  handleChange(rowIndex, cellIndex, e.target.value)
-                }
-                maxLength={1}
-                className={cn(`w-12 h-12 text-center border focus:outline-none focus:border-blue-500`,{"mr-2":thirdcell})}
-              />
+            const thirdRow = (rowIndex + 1) % 3 === 0;
+            const thirdCell = (cellIndex + 1) % 3 === 0;
+            const isReadOnly = sudokudata[rowIndex][cellIndex] !== "0";
+            return (
+              <>
+                <input
+                  key={`${rowIndex + 1}-${cellIndex + 1}`}
+                  type="text"
+                  value={cell === "0" ? "" : cell}
+                  readOnly={isReadOnly}
+                  onChange={(e) =>
+                    handleChange(rowIndex, cellIndex, e.target.value)
+                  }
+                  maxLength={1}
+                  className={cn(
+                    `w-12 h-12 col-span-2 text-center border border-error/50 focus:outline-none focus:border-blue-500`,
+                    {
+                      "bg-primary text-primary-content pointer-events-none":
+                        isReadOnly,
+                      "mb-3": thirdRow,
+                    }
+                  )}
+                />
+                {thirdCell ? <div className="opacity-0 w-1/2" /> : null}
+              </>
             );
           })
         )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "21": "repeat(21, minmax(0, 1fr))",
+      },
+    },
+  },
   plugins: [require("daisyui")],
   prefix: "",
   daisyui: {
-    themes: ["forest","cupcake"],
-    styled: true, 
+    themes: ["forest", "cupcake"],
+    styled: true,
   },
-  
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
