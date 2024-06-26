eca384291e3521610f85a260f73357471509f2c8
scshiv29-dev <shivchaudhary790@hotmail.com>
2024-06-27 02:33:15 +0530

    board edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <title>Sudoken</title>
       <body className={inter.className}>
       <Navbar/>
       {children}
