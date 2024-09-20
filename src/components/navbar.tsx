627b75f4996bc9c6fb8ffcc7b406175f7c458036
scshiv29-dev <shivchaudhary790@hotmail.com>
2024-09-20 15:42:46 +0530

    added routes for mygames and leaderboard

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import { SignIn } from "./sign-in";
 import { SignOut } from "./sign-out";
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 export default async function Navbar() {
   const session = await auth();
   if (session) {
@@ -18,14 +19,21 @@ export default async function Navbar() {
             <div
               tabIndex={0}
               role="button"
-              className="btn btn-ghost btn-circle avatar">
+              className="btn btn-ghost btn-circle avatar"
+            >
               <div className="w-10 rounded-full">
                 <UserAvatar />
               </div>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link href={"/mygames"} > 
+                <Button variant={"outline"} className="w-full"> My Games</Button>
+                </Link>
+              </li>
               <li>
                 <SignOut />
               </li>
@@ -42,11 +50,15 @@ export default async function Navbar() {
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <Menu className="h-8  w-8 text-white dark:text-base-100" />
           </div>
-        
         </div>
-        <Link href={"/"} className="btn btn-ghost text-white text-xl">Sudoken</Link>
+        <Link href={"/"} className="btn btn-ghost text-white text-xl">
+          Sudoken
+        </Link>
+     
+        <Link href={"/leaderboard"} className="btn btn-ghost text-white text-l">
+         LeaderBoard
+        </Link>
       </div>
-
 
       <div className="navbar-end space-x-2">
         <LoginLogout />
